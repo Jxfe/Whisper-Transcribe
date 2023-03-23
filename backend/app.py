b99f5781458617944701329a0397756dcda69a98
Jeff <majeff126@gmail.com>
2023-03-23 02:37:12 -0700

    Initial push

---
+++ backend/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import requests
+import base64
+import openai
+from io import BytesIO
+import json
+
+OPENAI_API_KEY = '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'
+openai.api_key = OPENAI_API_KEY
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/transcribe', methods=['POST'])
+def transcribe():
+    audio_file = request.files['audio']
+
+    # Transcribe audio using Whisper ASR API
+    audio_data = BytesIO(audio_file.read())
+    audio_data.name = audio_file.filename
+    response = openai.Audio.transcribe("whisper-1",file=audio_data)
+    data = str(response)
+    data = data[12:-2]
+    data = data[:-1]
+
+    return jsonify({"transcription": data})
+
+if __name__ == '__main__':
+    app.run(debug=True)
